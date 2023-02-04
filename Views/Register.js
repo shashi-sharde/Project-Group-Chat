7a672cf49a1c19aedd04bceb478d572492066a61
shashi-sharde <shashisharde@gmail.com>
2023-02-04 09:55:35 +0530

    User Signup(Registeration) Page

---
+++ Views/Register.js
@@ -0,0 +1,23 @@
+function userRegister(event){
+    event.preventDefault();
+    const Name = event.target.Name.value;
+    const Email = event.target.Email.value;
+    const Phone_Number = event.target.PhoneNumber.value;
+    const Password = event.target.Password.value
+
+    const obj = {Name,Email,Phone_Number,Password }
+
+    axios.post('http://localhost:3000/users/register',obj)
+    .then((response)=>{
+        if(response.status===201){
+            alert("Registration for new user is successfull!")
+            window.location.href='http://localhost:3000/Login.html'
+        }
+        else if(response.status===401){
+            document.body.innerHTML +=`<div style= 'color:red'> ${'Email Id already Registerd, please try to login!'}</div> `
+        }
+    }).catch(err =>{
+        console.log(err)
+    })
+
+}
